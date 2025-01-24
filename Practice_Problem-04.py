c75d3c0942b25ea8511f18ab510fba3896b1fd08
Suprio-Das <priyamd507@gmail.com>
2025-01-24 23:15:36 +0600

    Problem-6 added

---
+++ Practice_Problem-04.py
@@ -0,0 +1,9 @@
+def maxAndMin(number):
+    maxi = max(number)
+    mini = min(number)
+    return maxi, mini
+    
+array = [1, 2, 3, 4, 5]
+
+result = maxAndMin(array)
+print(result)
---
+++ Practice_Problem-06.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 23:13:42 2025
+
+@author: user
+"""
+
